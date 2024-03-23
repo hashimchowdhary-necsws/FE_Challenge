42c84da3addbe9c04bc0847c52fe3f8d33cc0c9b
hashimchowdhary-necsws <112179436+hashimchowdhary-necsws@users.noreply.github.com>
2024-03-23 18:49:29 +0530

    New Feature: design for email and phone number tab over search field and validation and also added style for responsiveness

--- js/app.js
+++ js/app.js
@@ -3,11 +3,14 @@ $(document).ready(function () {
   const loaderImg = '<img src="img/loading_spinner.gif" height="100" width="100" alt="loader"/><h5>Please wait a moment...</h5>';
   const loaderBody = document.getElementById('loader');
   const documentBody = document.querySelector('body');
+  //initialize constant values for inputs
+  const emailField = document.querySelector('input[type="text"][name="email"]');
+  const phoneField = document.querySelector('input[type="text"][name="phone"]');
 
   $("#btn-search").on("click", function (e) {
     e.preventDefault();
     localStorage.clear(); //Clears storage for next request
-    email = $('input[type="text"]').val().toLowerCase();
+    email = $('input[type="text"][name="email"]').val().toLowerCase();
 
     var x, y;
     regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
@@ -20,7 +23,7 @@ $(document).ready(function () {
     if (x === true) {
       loaderBody.innerHTML = loaderImg;
       documentBody.classList.add("loading");
-      document.querySelector('input[type="text"]').parentNode.classList.remove("error");
+      emailField.parentNode.classList.remove("error");
       const proxyurl = "";
       const url =
         'https://ltvdataapi.devltv.co/api/v1/records?email=' + email; //new updated API value
@@ -38,12 +41,18 @@ $(document).ready(function () {
     }
   });
 
-  $('input[type="text"]').keypress(function (event) {
-    email = $('input[type="text"]').val().toLowerCase();
-    regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-    if (email.match(regEx)) {
+  $('input[type="text"]').keyup(function (event) {
+    //email input value and reqularExpression
+    email = $('input[type="text"][name="email"]').val().toLowerCase();
+    regExEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+    //phone input value and reqularExpression
+    phone_number = $('input[type="text"][name="phone"]').val();
+    regExPhone = /^\d{10}$/;
+
+    if (email.match(regExEmail) || phone_number.match(regExPhone)) {
       x = true;
-      document.querySelector('input[type="text"]').parentNode.classList.remove("error");
+      this.parentNode.classList.remove("error");
     } else {
       x = false;
     }
@@ -63,9 +72,12 @@ $(document).ready(function () {
         loaderBody.innerHTML = loaderImg;
         documentBody.classList.add("loading");
         const proxyurl = "";
-        const url =
+
+        //if email is valid
+        if (email.match(regExEmail)) {
+          const url =
           'https://ltvdataapi.devltv.co/api/v1/records?email=' + email; //new updated API value
-        fetch(proxyurl + url)
+          fetch(proxyurl + url)
           .then((response) => response.text())
           .then(function (contents) {
             localStorage.setItem("userObject", contents);
@@ -74,9 +86,84 @@ $(document).ready(function () {
             documentBody.classList.remove("loading");
           })
           .catch((e) => console.log(e));
+        }
+        
+        //if phone mumber is valid
+        if (phone_number.match(regExPhone)) {
+          const url =
+          'https://ltvdataapi.devltv.co/api/v1/records?phone=' + phone_number; //new updated API value
+          fetch(proxyurl + url)
+          .then((response) => response.text())
+          .then(function (contents) {
+            localStorage.setItem("userObject", contents);
+            window.location.href = "result.html";
+            loaderBody.innerHTML = '';
+            documentBody.classList.remove("loading");
+          })
+          .catch((e) => console.log(e));
+        }
+
       } else if (x !== true) {
-        document.querySelector('input[type="text"]').parentNode.classList.add("error");
+        this.parentNode.classList.add("error");
       }
+    }
+  });
+
+  /**
+   * button search validation for phone number.
+  */
+  $("#btn-phone-search").on("click", function (e) {
+    e.preventDefault();
+    localStorage.clear(); //Clears storage for next request
+    phone_number = $('input[type="text"][name="phone"]').val();
+
+    var x, y;
+
+    regEx = /^\d{10}$/;
+    if (phone_number.match(regEx)) {
+      x = true;
+    } else {
+      x = false;
+    }
+
+    if (x === true) {
+      loaderBody.innerHTML = loaderImg;
+      documentBody.classList.add("loading");
+      phoneField.parentNode.classList.remove("error");
+      const proxyurl = "";
+        const url =
+        'https://ltvdataapi.devltv.co/api/v1/records?phone=' + phone_number; //new updated API value
+      fetch(proxyurl + url)
+        .then((response) => response.text())
+        .then(function (contents) {
+          localStorage.setItem("userObject", contents);
+          window.location.href = "result.html";
+          loaderBody.innerHTML = '';
+          documentBody.classList.remove("loading");
+        })
+        .catch((e) => console.log(e));
+    } else if (x !== true) {
+      phoneField.parentNode.classList.add("error");
+    }
+  });
+
+  /**
+   * Tab button for Email search and Phone search.
+  */
+  $(".btn-filter-search").on("click", function (e) {
+    $(".btn-filter-search").removeClass('active'); //remove class active
+    $(this).addClass('active'); //add class active on current
+    $('input[type="text"]').val(''); //clear input values
+
+    //get the attribute value of button to compare its related to email or phone
+    var attrClass = $(this).attr('data-value'); 
+
+    if (attrClass == 'email-search') {
+      $(".email-search").removeClass('display-none');
+      $(".phone-search").addClass('display-none');
+    } else {
+      $(".email-search").addClass('display-none');
+      $(".phone-search").removeClass('display-none');
     }
   });
 
